d194e76636a2291b240aee325578ca9c6b46c75d
akshay-ksd <akshaykarandakkad92@gmail.com>
2022-01-29 12:36:03 +0530

    admin

--- lib/pos_web/controllers/rest_controller.ex
+++ lib/pos_web/controllers/rest_controller.ex
@@ -43,11 +43,11 @@ defmodule PosWeb.RestController do
     u_token = "RLvzhl7YdqwpCmF9Qcd4qQpG3hfh9KPMr0gq5nnPv0wLgVs0IxCdozpQdEBJCTR6Iqh6GqV5OLERk"
     is_active = true
     number = "8157896995"
-    Staff.add_staff(access, uname, number, password, restaurent_token, u_token, is_active)
+    staff = Staff.add_staff(access, uname, number, password, restaurent_token, u_token, is_active)
     conn
     |> json(%{
       "is_registerd" => true,
-      "u_token" => u_token
+      "u_token" => staff
     })
   end
 
