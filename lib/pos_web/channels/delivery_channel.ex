51b4c471aee02e90fa0573e3980bca43c1d585f0
akshay-ksd <akshaykarandakkad92@gmail.com>
2022-01-31 12:18:06 +0530

    ll

--- lib/pos_web/channels/delivery_channel.ex
+++ lib/pos_web/channels/delivery_channel.ex
@@ -115,7 +115,7 @@ defmodule PosWeb.DeliveryChannel do
               for d <- 0..dcount-1, d >= 0  do
                 order_data = Enum.at(delivery, d)
                 Logger.info order_data
-                data_order = Enum.at(order_data, 5)
+                data_order = Enum.at(order_data, 7)
                 order_id = elem(data_order, 1)
                 Logger.info order_id
                 productDetails = Delivery.getDeliveryDetails(order_id)
