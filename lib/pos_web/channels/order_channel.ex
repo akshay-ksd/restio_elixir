863ae0892a808c90e461865e880b2cdec2c9de71
akshay-ksd <akshaykarandakkad92@gmail.com>
2022-02-08 15:55:17 +0530

    order details que

--- lib/pos_web/channels/order_channel.ex
+++ lib/pos_web/channels/order_channel.ex
@@ -126,13 +126,20 @@ defmodule PosWeb.OrderChannel do
             order_detail_id =  Enum.at(queue_data, i)
             order = Order.getOrderDetailsByDetailId(order_detail_id, restaurentId)
             ocount = Enum.count(order)
+
             if ocount !== 0 do
               for o <- 0..ocount-1, o >= 0  do
                 order_data = Enum.at(order, o)
-                Logger.info order_data
+                data_order = Enum.at(order_data, 1)
+                orderId = elem(data_order, 1)
+                order_master = OrderMaster.getOrderById(restaurentId, orderId)
+
+                broadcast!(socket, "checkQueue", %{"order" => order_master,"order_details" => order,"task" => task,"staffId" => staffId})
+                Logger.info order_master
               end
+            else
+              broadcast!(socket, "checkQueue", %{"order" => false,"order_details" => order,"task" => task,"staffId" => staffId})
             end
-            broadcast!(socket, "checkQueue", %{"order" => false,"order_details" => order,"task" => task,"staffId" => staffId})
           end
 
         task == "PRODUCT_DELETE" ->
