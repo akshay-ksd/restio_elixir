c509da144178011433b15466b384e5c2d6e41df4
akshay-ksd <akshaykarandakkad92@gmail.com>
2022-02-17 19:13:19 +0530

    windows

--- lib/pos_web/endpoint.ex
+++ lib/pos_web/endpoint.ex
@@ -12,8 +12,8 @@ defmodule PosWeb.Endpoint do
 
   socket "/socket", PosWeb.UserSocket,
     websocket: true,
-    longpoll: false
-
+    longpoll: false,
+    check_origin: false
   socket "/live", Phoenix.LiveView.Socket, websocket: [connect_info: [session: @session_options]]
 
   # Serve at "/" the static files from "priv/static" directory.
