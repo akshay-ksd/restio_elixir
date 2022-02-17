dd1712db04a6b97bdf5ca7577ce6e96e264c17eb
akshay-ksd <akshaykarandakkad92@gmail.com>
2022-02-17 15:45:56 +0530

    windows

--- config/prod.exs
+++ config/prod.exs
@@ -15,7 +15,8 @@ config :pos, PosWeb.Endpoint,
   server: true,
   secret_key_base: "${SECRET_KEY_BASE}",
 
-  url: [scheme: "https", host: "prickly-flawed-pronghorn.gigalixirapp.com", port: 443],
+  # url: [scheme: "https", host: "prickly-flawed-pronghorn.gigalixirapp.com", port: 443],
+  url: [host: "prickly-flawed-pronghorn.gigalixirapp.com", port: 443, scheme: "https"]
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
