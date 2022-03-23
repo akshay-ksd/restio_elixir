a22eaa1efc172f9a2f0d20d8570d0686a375519f
akshay-ksd <akshaykarandakkad92@gmail.com>
2022-03-23 10:45:37 +0530

    order

--- lib/pos_web/channels/order_channel.ex
+++ lib/pos_web/channels/order_channel.ex
@@ -199,16 +199,11 @@ defmodule PosWeb.OrderChannel do
                     orderId = elem(data_order, 1)
                     order_details_data = Order.getOrderDetailsById(restaurentId, orderId)
 
-                    s_data = %{"data" => order_data,"order_details_data" => order_details_data,"empty" => false}
+                    s_data = %{"data" => order_data,"order_details_data" => order_details_data}
                     broadcast!(socket, "getOrder", %{"data" => s_data})
-
-                    if count-1 == o do
-                      s_data = %{"data" => false,"empty" => true}
-                      broadcast!(socket, "getOrder", %{"data" => s_data})
-                    end
                 end
             else
-                s_data = %{"data" => false,"empty" => false}
+                s_data = %{"data" => false}
                 broadcast!(socket, "getOrder", %{"data" => s_data})
             end
     {:noreply, socket}
