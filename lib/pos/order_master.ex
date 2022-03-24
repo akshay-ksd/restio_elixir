16ec55c89acb5531e24d2298513633d619996aa5
akshay-ksd <akshaykarandakkad92@gmail.com>
2022-03-24 11:47:47 +0530

    date

--- lib/pos/order_master.ex
+++ lib/pos/order_master.ex
@@ -4,7 +4,6 @@ defmodule Pos.OrderMaster do
   alias Pos.OrderMaster
   alias Pos.Queue
   alias Pos.Staff
-  alias Logger
 
   schema "order_master" do
     field :date, :string
@@ -113,7 +112,7 @@ defmodule Pos.OrderMaster do
                     user_id: p.user_id, gst: p.gst, charge: p.charge, tableNumber: p.tableNumber, order_date: p.order_date, s_gst: p.s_gst, id: p.id})
           |> Pos.Repo.all()
         else
-          from(p in OrderMaster, where: p.restaurent_id == ^restaurentId and p.status == 4,
+          from(p in OrderMaster, where: fragment("?::date", p.inserted_at) > ^date and p.restaurent_id == ^restaurentId,
           order_by: fragment("? DESC", p.inserted_at), limit: ^limit, offset: ^offset,
           select: %{order_id: p.order_id, date: p.date, restaurent_id: p.restaurent_id, status: p.status, time: p.time,
                     user_id: p.user_id, gst: p.gst, charge: p.charge, tableNumber: p.tableNumber, order_date: p.order_date, s_gst: p.s_gst, id: p.id})
