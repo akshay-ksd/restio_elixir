9c99ac1eef795e06a09b5035b561f48157881950
akshay-ksd <akshaykarandakkad92@gmail.com>
2022-03-29 12:42:43 +0530

    get

--- lib/pos/order_master.ex
+++ lib/pos/order_master.ex
@@ -64,23 +64,23 @@ defmodule Pos.OrderMaster do
     |> Ecto.Changeset.change(%{status: status})
     |> Pos.Repo.update()
 
-    access1 = "ALL"
-    access2 = "ORDER"
+    # access1 = "ALL"
+    # access2 = "ORDER"
 
-    accessid = order_id
-    restaurentId = restaurent_id
-    section = "Order"
-    task = "UPDATE"
+    # accessid = order_id
+    # restaurentId = restaurent_id
+    # section = "Order"
+    # task = "UPDATE"
 
-    Queue.deleteOldUpdate(restaurentId, section, task, accessid)
+    # Queue.deleteOldUpdate(restaurentId, section, task, accessid)
 
-    staffData = Staff.getTokenByAccess(restaurent_id, access1, access2)
-    count = Enum.count(staffData)
+    # staffData = Staff.getTokenByAccess(restaurent_id, access1, access2)
+    # count = Enum.count(staffData)
 
-    for i <- 0..count-1, i >= 0 do
-      staffId = Enum.at(staffData, i)
-      Queue.addQueueData(accessid, restaurentId, section, staffId, task, time)
-    end
+    # for i <- 0..count-1, i >= 0 do
+    #   staffId = Enum.at(staffData, i)
+    #   Queue.addQueueData(accessid, restaurentId, section, staffId, task, time)
+    # end
   end
 
   def updateOrderData(order_id,restaurent_id,gst,charge,tableNumber,order_date,s_gst) do
