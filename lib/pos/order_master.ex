05160e2c458d1423efb03680d8bd915e85b01175
akshay-ksd <akshaykarandakkad92@gmail.com>
2022-04-02 19:10:19 +0530

    data

--- lib/pos/order_master.ex
+++ lib/pos/order_master.ex
@@ -174,7 +174,7 @@ defmodule Pos.OrderMaster do
   end
 
   def getOrderByDate(restaurentId,dateStart,dateEnd) do
-    from(p in OrderMaster, where: p.restaurent_id == ^restaurentId and p.time >= ^dateStart and p.time <= ^dateEnd and p.status >0 and p.status < 4,
+    from(p in OrderMaster, where: p.restaurent_id == ^restaurentId and p.time >= ^dateStart and p.time <= ^dateEnd and p.status == 0,
     select: %{total: p.total, date: p.date, gst: p.gst, charge: p.charge, order_date: p.order_date, id: p.id, time: p.time})
     |> Pos.Repo.all()
   end
