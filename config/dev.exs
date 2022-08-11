864e77acd5224625f7f0a49a912616694cae9d6a
akshay-ksd <akshaykarandakkad92@gmail.com>
2022-08-12 00:09:37 +0530

    jjs

--- config/dev.exs
+++ config/dev.exs
@@ -19,7 +19,7 @@ config :pos, PosWeb.Endpoint,
   http: [port: 4000],
   debug_errors: true,
   code_reloader: true,
-  check_origin: false,
+  check_origin: true,
   watchers: [
     node: [
       "node_modules/webpack/bin/webpack.js",
