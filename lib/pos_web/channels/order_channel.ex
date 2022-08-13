152fd2d87bf17de84725bfbf109e1404842a7b41
akshay-ksd <akshaykarandakkad92@gmail.com>
2022-08-13 07:56:24 +0530

    jjs

--- lib/pos_web/channels/order_channel.ex
+++ lib/pos_web/channels/order_channel.ex
@@ -180,6 +180,7 @@ defmodule PosWeb.OrderChannel do
 
     date = data["date"]
 
+
     order_master_data = OrderMaster.getOrderByPagination(restaurentId,limit,offset,filterType,date)
 
     count = Enum.count(order_master_data)
@@ -190,7 +191,7 @@ defmodule PosWeb.OrderChannel do
                     data_order = Enum.at(order_data, 5)
                     orderId = elem(data_order, 1)
                     order_details_data = Order.getOrderDetailsById(restaurentId, orderId)
-
+                    Logger.info data_order
                     s_data = %{"data" => order_data,"order_details_data" => order_details_data}
                     broadcast!(socket, "getOrder", %{"data" => s_data})
                 end
