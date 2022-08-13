089e2a8e80e4255cedd3613f5b3714be9bfd2967
akshay-ksd <akshaykarandakkad92@gmail.com>
2022-08-13 08:15:06 +0530

    jjs

--- lib/pos_web/channels/order_channel.ex
+++ lib/pos_web/channels/order_channel.ex
@@ -191,7 +191,8 @@ defmodule PosWeb.OrderChannel do
                     data_order = Enum.at(order_data, 5)
                     orderId = elem(data_order, 1)
                     order_details_data = Order.getOrderDetailsById(restaurentId, orderId)
-                    Logger.info(order_data)
+                    allData = [order_data | order_details_data]
+                    Logger.info(allData)
                     s_data = %{"data" => order_data,"order_details_data" => order_details_data}
                     broadcast!(socket, "getOrder", %{"data" => s_data})
                 end
