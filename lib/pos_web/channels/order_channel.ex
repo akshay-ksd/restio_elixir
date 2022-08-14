a3e2e473d60112eddddcb7801a5570ffc7c986bf
akshay-ksd <akshaykarandakkad92@gmail.com>
2022-08-14 15:51:11 +0530

    jjs

--- lib/pos_web/channels/order_channel.ex
+++ lib/pos_web/channels/order_channel.ex
@@ -190,18 +190,22 @@ defmodule PosWeb.OrderChannel do
                     data_order = Enum.at(order_data, 5)
                     orderId = elem(data_order, 1)
                     order_details_data = Order.getOrderDetailsById(restaurentId, orderId)
-
+                    List.insert_at(order_master_data, -1, order_details_data)
                     s_data = %{"data" => order_data,"order_details_data" => order_details_data}
                     broadcast!(socket, "getOrder", %{"data" => s_data})
+
+                    if o == count-1 do
+                      Logger.info(order_master_data)
+                    end
                 end
             else
                 s_data = %{"data" => false}
                 broadcast!(socket, "getOrder", %{"data" => s_data})
             end
-            map = Enum.reduce order_master_data, %{}, fn x, acc ->
-              Map.put(acc, x, x)
-              Logger.info(x)
-            end
+            # map = Enum.reduce order_master_data, %{}, fn x, acc ->
+            #   Map.put(acc, x, x)
+            # end
+            # Logger.info(map)
     {:noreply, socket}
   end
 
